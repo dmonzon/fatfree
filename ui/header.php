2e11297937b05d00688d517a272a9c5557e71df5
Danny Monzon <dmonzon@gmail.com>
2021-07-01 14:34:01 -0400

    testing

--- ui/header.php
+++ ui/header.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo $f3->get('BASE');?>/ui/css/style.css">
+</head>
 <html>
 <div  style="text-align: center;font-size:x-large;">
     <a href="<?php echo $f3->get('BASE');?>/jobs/" target="_self"><label>Jobs</label></a>
